f8a83b29b6065796f95e0a62b41ba4d35a19ddb0
dhanukaprr <dhanukaprr1@gmail.com>
2021-03-06 16:19:04 +0530

    New Site
    New Site

---
+++ template-parts/header-styles.php
@@ -0,0 +1,24 @@
+<!-- header styles -->
+
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+<style>.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 72px;
+}
+.u-header .u-menu-1 {
+  margin: 20px auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+}
+.u-block-60d9-19 {
+  font-size: 1rem;
+}
+.u-header .u-nav-2 {
+  font-size: 1rem;
+}
+.u-block-60d9-17 {
+  font-size: 1rem;
+}</style>
